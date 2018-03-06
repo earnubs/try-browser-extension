ef935df90991efa8629fb704b39f0d9882ed012e
Stephen Stewart <stephen.stewart@carisenda.com>
2018-03-06 15:32:03 +0000

    minor tidy up

--- background.js
+++ background.js
@@ -1,8 +1,21 @@
-function cancelIfTracker(details) {
-  return {cancel: details.url.indexOf("analytics.js") != -1};
+function handleBeforeRequest(details) {
+  const {url, tabId} = details;
+
+  // -1 indicates a request unrelated to the tab
+  if (tabId === -1) {
+    return;
+  }
+  // https://developer.chrome.com/extensions/webRequest#type-BlockingResponse
+  return {cancel: isTracker(url)};
+}
+
+function isTracker(url) {
+  // simple check for google analytics tracker ...
+  return url.indexOf("analytics.js") != -1;
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
-  cancelIfTracker,
+  handleBeforeRequest,
   {urls: ["<all_urls>"]},
+  // callback will be syncronous, and can return a blockingResponse
   ["blocking"]);
